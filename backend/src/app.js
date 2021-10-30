77aa74be623e0a01f985912a1ff4fd49bfeda7d7
Marin Capan <marin.capan@fer.hr>
2021-10-30 19:00:31 +0200

    added .env to gitignore

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const dotenv = require('dotenv');
+dotenv.config();
 //Create an app
 const app = express();
 
