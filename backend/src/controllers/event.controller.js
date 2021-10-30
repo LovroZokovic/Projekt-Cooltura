479099c95bd47059d176aa1af4d43fc79366ec2f
Marin Capan <marin.capan@fer.hr>
2021-10-30 19:25:04 +0200

    finished event endpoints in controller

--- backend/src/controllers/event.controller.js
+++ backend/src/controllers/event.controller.js
@@ -4,7 +4,32 @@ const Op = db.Sequelize.Op;
 
 // Create and Save a new Event
 exports.create = (req, res) => {
-  
+  // Validate request
+  if (!req.body.title) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  // Create a Tutorial
+  const event = {
+    title: req.body.title,
+    description: req.body.description,
+    interest: 0
+  };
+
+  // Save Tutorial in the database
+  Event.create(event)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Event."
+      });
+    });
 };
 
 // Retrieve all Events from the database.
@@ -24,7 +49,7 @@ exports.findAll = (req, res) => {
 
 // Find a single Event with an id
 exports.findOne = (req, res) => {
-  const id = 4;
+  const id = req.params.id;
 
   Event.findByPk(id)
     .then(data => {
@@ -45,16 +70,69 @@ exports.findOne = (req, res) => {
 
 // Update a Event by the id in the request
 exports.update = (req, res) => {
-  
+  const id = req.params.id;
+
+  Event.update(req.body, {
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Event was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update Event with id=${id}. Maybe Event was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Event with id=" + id
+      });
+    });
 };
 
 // Delete a Event with the specified id in the request
 exports.delete = (req, res) => {
-  
+  const id = req.params.id;
+
+  Event.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Event was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Event with id=${id}. Maybe Event was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Event with id=" + id
+      });
+    });
+
 };
 
 // Delete all Events from the database.
 exports.deleteAll = (req, res) => {
-  
+  Event.destroy({
+    where: {},
+    truncate: false
+  })
+    .then(nums => {
+      res.send({ message: `${nums} Events were deleted successfully!` });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all events."
+      });
+    });
 };
 
