58f83d95c9038b51ca9d8c8a7dbd5af74a7d6be6
LovroZokovic <lovro.zokovic@fer.hr>
2022-01-11 11:40:15 +0100

    Stvaranje novog brancha

---
+++ frontend/src/routes/Router.js
@@ -0,0 +1,32 @@
+import { lazy } from "react";
+import { Navigate } from "react-router-dom";
+
+/****Layouts*****/
+const FullLayout = lazy(() => import("../layouts/FullLayout.js"));
+
+/***** Pages ****/
+
+const Starter = lazy(() => import("../views/Starter.js"));
+const About = lazy(() => import("../views/About.js"));
+const Alerts = lazy(() => import("../views/ui/Alerts"));
+const Badges = lazy(() => import("../views/ui/Badges"));
+const Buttons = lazy(() => import("../views/ui/Buttons"));
+const Cards = lazy(() => import("../views/ui/Cards"));
+const Grid = lazy(() => import("../views/ui/Grid"));
+const Tables = lazy(() => import("../views/ui/Tables"));
+const Forms = lazy(() => import("../views/ui/Forms"));
+const Breadcrumbs = lazy(() => import("../views/ui/Breadcrumbs"));
+
+/*****Routes******/
+
+const ThemeRoutes = [
+  {
+    path: "/",
+    element: <FullLayout />,
+    children: [
+      { path: "/about", exact: true, element: <About /> }
+    ],
+  },
+];
+
+export default ThemeRoutes;
