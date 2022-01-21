76fad4035ebcafee9e3db5a2892fa726d3a1c094
Marin Capan <marin.capan@gmail.com>
2022-01-21 09:55:38 +0100

    fixed single event showcase

--- backend/src/app.js
+++ backend/src/app.js
@@ -62,11 +62,8 @@ app.use(logger);
 
 app.use(
   session({
-    // Key we want to keep secret which will encrypt all of our information
     secret: process.env.SECRET,
-    // Should we resave our session variables if nothing has changes which we dont
     resave: false,
-    // Save empty value if there is no vaue which we do not want to do
     saveUninitialized: false,
     cookie: {secure: false}
   })
--- frontend/src/views/ui/Events.js
+++ frontend/src/views/ui/Events.js
@@ -3,38 +3,27 @@ import {
 } from "reactstrap";
 import BigCard from "../../components/dashboard/BigCard";
 import axios from "axios";
+import {useParams} from "react-router-dom"
+import React,{useState,useEffect} from 'react'
 
-let event = {
-    id: "",
-    title: "",
-    description: "",
-    date: new Date(2021, 8, 25).toDateString(),
-    image: "",
-
-}
-
-//const EventsData =
-  //  {
-   //     image: e4,
-   //     title: "Technical museum",
-    //    subtitle: "Remembering world war 2",
-    //    description: "Join us on this historical journey related to all interested things happened in the world war 2. Stories about Nazi Germany, Holocaust, Soviets. We will depict you all the horrors of the war through our presentations and curious war possessions that we own, war tanks, planes, guns, mines, uniforms. Come and see it for yourself",
-   //     color: "primary",
-   //     date: "11.2.2022.",
-    //    time: "17:00",
-  //  };
-
-function getEvent() {
-    axios.get("https://localhost:2080/api/events/:id").
-        then((response) => {
-            console.log(response);
-            event = response.data;
-    });
-}
-
-getEvent();
+const useFetch = url => {
+    const [data, setData] = useState(null);
+  
+    async function fetchData() {
+      const response = await axios.get(url);
+      setData(response.data);
+    }
+  
+    useEffect(() => {fetchData()},[url]);
+    return data;
+  };
 
 const Events = () => {
+    const { id } = useParams()
+    const event = useFetch("http://localhost:2080/api/events/"+id)
+    if (!event) {
+        return <div>Loading...</div>
+    }
     return(
 
         <div>
